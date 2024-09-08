94cedaf4c5686d0b46a2d160f9e2f16c7ca28ea4
Andrei <cmihaiandrei1995@gmail.com>
2024-09-08 15:05:23 +0300

    stable?I guess

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const helmet = require('helmet');
 const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
 const routes = require('./routes/v1');
-const app = express();
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
 const httpStatus = require('http-status');
+const config = require('./config/config');
+const morgan = require('./config/morgan');
 const passport = require('passport');
 const { jwtStrategy } = require('./config/passport');
+const { authLimiter } = require('./middlewares/rateLimiter');
+
+const app = express();
+if (config.env !== 'test') {
+    app.use(morgan.successHandler);
+    app.use(morgan.errorHandler);
+}
+
+// set security HTTP headers
+app.use(helmet());
 
 // parse json request body
 app.use(express.json());
@@ -17,9 +31,21 @@ app.use(express.urlencoded({ extended: true }));
 // sanitize request data
 app.use(xss());
 
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
+
+// limit repeated failed requests to auth endpoints
+if (config.env === 'production') {
+    app.use('/v1/auth', authLimiter);
+}
 
 // v1 api routes
 app.use(routes);
--- routes/v1/base.route.js
+++ routes/v1/base.route.js
@@ -6,9 +6,12 @@ const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
+/**
+ * router.get('/path', middlewares, controller.method);
+ */
 router.get('/', mainController.index);
 router.get('/users', mainController.getUsers);
 router.get('/users/create', mainController.createUser);
-router.get('/users/:id', auth(), mainController.getUserById);
+router.get('/users/:id', mainController.getUserById);
 
 module.exports = router;
